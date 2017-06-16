5d9a52a928cef5a91bf8cb01fba2029c6330b48e
jixge <jixge@microsoft.com>
2017-06-16 17:33:43 +0800

    add check function
    containNum
    containSpecial

--- DataAnalysisEngine/DataAnalysis.cs
+++ DataAnalysisEngine/DataAnalysis.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace DataAnalysisEngine
@@ -35,14 +36,24 @@ namespace DataAnalysisEngine
             return double.TryParse(input, out output);
         }
 
-        private bool ContainNumber()
+        private bool ContainNumber(string input)
         {
-            
+            //return input.Any(t => char.IsDigit(t));
+            return input.All(char.IsDigit);
         }
 
-        private bool ContainSpecialLetter()
+        private bool ContainSpecialLetters(string input, char[] specialLetters)
         {
-            
+            Regex pattern = new Regex(@"^[a-zA-Z0-9_@.-]*$");
+            return pattern.IsMatch(input);
+
+            //int indexOf = input.IndexOfAny(specialLetters);
+            //return indexOf == -1;
+        }
+
+        private bool ContainWords(string input, string word)
+        {
+            return input.Contains(word);
         }
     }
 }
